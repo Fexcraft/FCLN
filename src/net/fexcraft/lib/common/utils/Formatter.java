6e161a606740aa33937645f501f4ac4b2d6627f0
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-23 09:13:10 +0200

    Update Formatter.java

--- src/net/fexcraft/lib/common/utils/Formatter.java
+++ src/net/fexcraft/lib/common/utils/Formatter.java
@@ -8,13 +8,18 @@ public class Formatter {
 	public static final String PARAGRAPH_SIGN = "\u00a7";
 	
 	public static String format(String string){
-		if(string == null){ return ""; }
-		string = string.replaceAll("&([a-f0-9k-or])", "\u00a7$1");
-		return string;
+		if(string == null) return "";
+		return string.replaceAll("&([a-f0-9k-or])", "\u00a7$1");
 	}
 
 	public static String format(String string, Object... objs){
 		return format(String.format(string, objs));
 	}
+
+	public static String clear(String string){
+		if(string == null) return "";
+		return string.replaceAll("&([a-f0-9k-or])", "$1");
+	}
+
 	
 }
