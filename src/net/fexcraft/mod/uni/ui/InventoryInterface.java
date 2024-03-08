4bf11ffeb64aeb65f2ad57cab59d72abe40b6d9f
Ferdinand Calo <fexcalo@gmail.com>
2024-03-08 23:27:27 +0100

    Create InventoryInterface.java

---
+++ src/net/fexcraft/mod/uni/ui/InventoryInterface.java
@@ -0,0 +1,26 @@
+package net.fexcraft.mod.uni.ui;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.uni.item.StackWrapper;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.EntityW;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public abstract class InventoryInterface extends ContainerInterface {
+
+	public InventoryInterface(JsonMap map, EntityW ply, V3I pos){
+		super(map, ply, pos);
+	}
+
+	public abstract Object getInventory();
+
+	public abstract void setInventoryContent(int index, TagCW com);
+
+	public abstract StackWrapper getInventoryContent(int index);
+
+	public abstract boolean isInventoryEmpty(int at);
+
+}
