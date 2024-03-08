8a127ba1368aa0ea98f5523a7b23cafb519e1d64
Ferdinand Calo <fexcalo@gmail.com>
2024-03-08 23:27:48 +0100

    Update UITab.java

--- src/net/fexcraft/mod/uni/ui/UITab.java
+++ src/net/fexcraft/mod/uni/ui/UITab.java
@@ -14,6 +14,7 @@ public abstract class UITab extends UIButton {
 	public LinkedHashMap<String, UIText> texts = new LinkedHashMap<>();
 	public LinkedHashMap<String, UIButton> buttons = new LinkedHashMap<>();
 	public LinkedHashMap<String, UIField> fields = new LinkedHashMap<>();
+	public LinkedHashMap<String, UISlot> slots = new LinkedHashMap<>();
 
 	public UITab(UserInterface ui, JsonMap map) throws Exception {
 		super(ui, map);
@@ -30,6 +31,11 @@ public abstract class UITab extends UIButton {
 		if(map.has("fields")){
 			map.getArray("fields").value.forEach(json -> {
 				fields.put(json.string_value(), ui.fields.get(json.string_value()));
+			});
+		}
+		if(map.has("slots")){
+			map.getArray("slots").value.forEach(json -> {
+				slots.put(json.string_value(), ui.slots.get(json.string_value()));
 			});
 		}
 	}
