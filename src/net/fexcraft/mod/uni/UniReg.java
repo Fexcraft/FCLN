0e79e8b46f4252751c080196c738de39c9c0cc1e
Ferdinand Calo <fexcalo@gmail.com>
2024-06-28 23:00:40 +0200

    Update UniReg.java

--- src/net/fexcraft/mod/uni/UniReg.java
+++ src/net/fexcraft/mod/uni/UniReg.java
@@ -1,9 +1,11 @@
 package net.fexcraft.mod.uni;
 
 import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.ui.UIKey;
 import net.fexcraft.mod.uni.ui.UserInterface;
 
 import java.util.LinkedHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -11,21 +13,30 @@ import java.util.LinkedHashMap;
 public class UniReg {
 
 	public static String LOADER_VERSION = null;
-	public static LinkedHashMap<String, Class<? extends UserInterface>> GUI = new LinkedHashMap<>();
-	public static LinkedHashMap<String, Class<? extends ContainerInterface>> MENU = new LinkedHashMap<>();
-	public static LinkedHashMap<String, String> MENU_JSON = new LinkedHashMap<>();
+	public static ConcurrentHashMap<String, Object> INSTANCES = new ConcurrentHashMap<>();
+	public static LinkedHashMap<UIKey, Class<? extends UserInterface>> GUI = new LinkedHashMap<>();
+	public static LinkedHashMap<UIKey, Class<? extends ContainerInterface>> MENU = new LinkedHashMap<>();
+	public static LinkedHashMap<UIKey, String> MENU_JSON = new LinkedHashMap<>();
 
-	public static boolean registerUI(String key, Class<? extends UserInterface> ui){
+	public static void registerMod(String id, Object inst){
+		INSTANCES.put(id, inst);
+	}
+
+	public static boolean registerUI(UIKey key, Class<? extends UserInterface> ui){
 		if(GUI.containsKey(key)) return false;
 		GUI.put(key, ui);
 		return true;
 	}
 
-	public static boolean registerMenu(String key, String loc, Class<? extends ContainerInterface> con){
+	public static boolean registerMenu(UIKey key, String loc, Class<? extends ContainerInterface> con){
 		if(MENU.containsKey(key)) return false;
 		MENU.put(key, con);
 		MENU_JSON.put(key, loc);
 		return true;
+	}
+
+	public static Object getInst(UIKey key){
+		return INSTANCES.get(key.key.substring(0, key.key.indexOf(":")));
 	}
 
 }
