750468375883c53362aa9ff79e4c236f13e8818a
Ferdinand Calo <fexcalo@gmail.com>
2024-07-09 19:24:10 +0200

    Update UniEntity.java

--- src/net/fexcraft/mod/uni/UniEntity.java
+++ src/net/fexcraft/mod/uni/UniEntity.java
@@ -1,10 +1,8 @@
 package net.fexcraft.mod.uni;
 
-import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.world.EntityW;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.function.Function;
 
 /**
@@ -14,95 +12,36 @@ public class UniEntity {
 
 	public static Function<Object, UniEntity> GETTER = null;
 	public static Function<Object, EntityW> ENTITY_GETTER = null;
-	private static ArrayList<Class<? extends AppData>> pclasses = new ArrayList<>();
-	private static ArrayList<Class<? extends AppData>> eclasses = new ArrayList<>();
-	private HashMap<Class<? extends AppData>, AppData> appended = new HashMap<>();
+	private static ArrayList<Appendable<UniEntity>> appendables = new ArrayList<>();
+	public final Appended<UniEntity> appended = new Appended(this);
 	public EntityW entity;
 
 	public UniEntity(){}
 
 	public UniEntity set(Object ent){
 		entity = ENTITY_GETTER.apply(ent);
-		if(entity.isPlayer()){
-			for(Class<? extends AppData> clazz : pclasses){
-				try{
-					appended.put(clazz, clazz.newInstance());
-				}
-				catch(InstantiationException | IllegalAccessException e){
-					throw new RuntimeException(e);
-				}
-			}
-		}
-		for(Class<? extends AppData> clazz : eclasses){
-			try{
-				appended.put(clazz, clazz.newInstance());
-			}
-			catch(InstantiationException | IllegalAccessException e){
-				throw new RuntimeException(e);
-			}
-		}
-		for(AppData val : appended.values()){
-			val.init(this);
-		}
+		appended.init(appendables);
 		return this;
-	}
-
-	public void save(TagCW com){
-		for(AppData val : appended.values()){
-			TagCW tag = TagCW.create();
-			val.save(this, tag);
-			if(!tag.empty()){
-				com.set(val.id(), tag);
-			}
-		}
-	}
-
-	public void load(TagCW com){
-		for(AppData val : appended.values()){
-			if(com.has(val.id())){
-				val.load(this, com.getCompound(val.id()));
-			}
-		}
 	}
 
 	public static UniEntity get(Object playerent){
 		return GETTER.apply(playerent);
 	}
 
+	public <A> A getApp(Class<A> clazz){
+		return appended.get(clazz);
+	}
+
+	public <A> A getApp(String id){
+		return appended.get(id);
+	}
+
 	public static EntityW getEntity(Object playerent){
 		return GETTER.apply(playerent).entity;
 	}
 
-	public <AD extends AppData> AD get(String id){
-		for(AppData val : appended.values()){
-			if(val.id().equals(id)) return (AD)val;
-		}
-		return null;
-	}
-
-	public <AD extends AppData> AD get(Class<AD> clazz){
-		return (AD)appended.get(clazz);
-	}
-
-	public static void register(Class<? extends AppData> clazz, boolean playeronly){
-		if(playeronly){
-			if(!pclasses.contains(clazz)) pclasses.add(clazz);
-		}
-		else{
-			if(!eclasses.contains(clazz)) eclasses.add(clazz);
-		}
-	}
-
-	public static interface AppData {
-
-		public void save(UniEntity player, TagCW com);
-
-		public void load(UniEntity player, TagCW com);
-
-		public void init(UniEntity uniPlayer);
-
-		public String id();
-
+	public static void register(Appendable<UniEntity> app){
+		appendables.add(app);
 	}
 
 }
