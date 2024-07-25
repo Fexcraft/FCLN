38e54a2f907c29b3e0605396bfafe9a6a9aafc3e
Ferdinand Calo <fexcalo@gmail.com>
2024-07-25 14:50:46 +0200

    Update IDLManager.java

--- src/net/fexcraft/mod/uni/IDLManager.java
+++ src/net/fexcraft/mod/uni/IDLManager.java
@@ -1,5 +1,7 @@
 package net.fexcraft.mod.uni;
 
+import net.fexcraft.mod.uni.impl.IDLM;
+
 /**
  * 
  * @author Ferdinand Calo' (FEX___96)
@@ -7,35 +9,35 @@ package net.fexcraft.mod.uni;
  */
 public interface IDLManager {
 	
-	public static IDLManager[] INSTANCE = { null };
+	public static IDLManager INSTANCE = new IDLM();
 
 	public IDL get(String idl, boolean cache, boolean named);
 	
 	public static IDL getIDL(String idl, boolean cache, boolean named){
-		return INSTANCE[0].get(idl, cache, named);
+		return INSTANCE.get(idl, cache, named);
 	}
 	
 	public static IDL getIDLCached(String idl, boolean named){
-		return INSTANCE[0].get(idl, true, named);
+		return INSTANCE.get(idl, true, named);
 	}
 	
 	public static IDL getIDLNamed(String idl, boolean cache){
-		return INSTANCE[0].get(idl, cache, true);
+		return INSTANCE.get(idl, cache, true);
 	}
 	
 	/** Cached and unnamed. */
 	public static IDL getIDLCached(String idl){
-		return INSTANCE[0].get(idl, true, false);
+		return INSTANCE.get(idl, true, false);
 	}
 
 	/** Cached and named. */
 	public static IDL getIDLNamed(String idl){
-		return INSTANCE[0].get(idl, true, true);
+		return INSTANCE.get(idl, true, true);
 	}
 
 	/** Not cached and unnamed. */
 	public static IDL getIDL(String idl){
-		return INSTANCE[0].get(idl, false, false);
+		return INSTANCE.get(idl, false, false);
 	}
 
 }
